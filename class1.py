05d0da61142207c14d1358545589c8dd1e0ddf37
David <david.zeller@outlook.com>
2020-02-21 20:35:08 +0100

    a

---
+++ class1.py
@@ -0,0 +1,5 @@
+def func1(i):
+    if i > 50:
+        print("a")
+    else:       
+        print("Hello, World!")
---
+++ test.py
@@ -0,0 +1,7 @@
+import class1
+
+i=0
+n=100
+while i <= n:
+    class1.func1(i)
+    i = i+1
